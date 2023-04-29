ed48023a0a9aaee11a842941c2a3edd6ee7af977
avadoe <aashuaripirala@gmail.com>
2023-04-29 21:36:05 +0530

    10 split + neuronsum=100or150

--- mnist.py
+++ mnist.py
@@ -40,9 +40,9 @@ kf = KFold(n_splits=10, shuffle=True)
 
 
 
-for train_idx, test_idx in kf.split(X_train):
-    X_cv_train, X_cv_test = X_train[train_idx], X_train[test_idx]
-    y_cv_train, y_cv_test = y_train[train_idx], y_test[test_idx]
+for train_index, test_index in kf.split(X_train):
+    X_cv_train, X_cv_test = X_train[train_index], X_train[test_index]
+    y_cv_train, y_cv_test = y_train[train_index], y_train[test_index]
     for i, architecture in enumerate(architectures):
         if architecture['num_hidden_layers'] == 2:
             num_neurons = [50, 50] if random.random() == 0.5 else [30, 70] if random.random() < 0.5 else [70, 30]
@@ -50,7 +50,6 @@ for train_idx, test_idx in kf.split(X_train):
             num_neurons = [25, 25, 50] if random.random() == 0.5 else [25, 50, 25] if random.random() < 0.5 else [50, 25, 25]
             
         architecture['hidden_layer_size'] = num_neurons
-        
         for activation in activation_functions:
             for output_fn in output_functions:
                 model = Sequential()
@@ -61,18 +60,9 @@ for train_idx, test_idx in kf.split(X_train):
                     model.add(Dense(units=num_neurons[i], activation=activation))
                 model.add(Dense(units=num_classes, activation=output_fn))               
                 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) 
-                model.fit(X_train, y_train, batch_size=batch_size, epochs=num_epochs, verbose=0)
-                y_predict = np.argmax(model.predict(X_test), axis=1)
-                accuracy = accuracy_score(np.argmax(y_test, axis=1), y_pred=y_predict)
-                cm = confusion_matrix(np.argmax(y_test, axis=1), y_pred=y_predict)
-                
-                results.append({
-                    'architecture': architecture,
-                    'activation function': activation,
-                    'accuracy': accuracy,
-                    'confusion matrix': cm,
-                    'output function': output_fn
-                })
+                model.fit(X_cv_train, y_cv_train, batch_size=batch_size, epochs=num_epochs, verbose=0)
+                y_predict = np.argmax(model.predict(X_cv_test), axis=1)
+                accuracy = accuracy_score(np.argmax(y_cv_test, axis=1), y_pred=y_predict)
             
 for i, result in enumerate(results):
     print(f'Model {i+1}: Architecture={result["architecture"]}, Activation Function={result["activation function"]}, Output Function={result["output function"]},Accuracy={result["accuracy"]:.4f}')
