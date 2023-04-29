774b980de05850eb036d9cd5a99c159e04971058
avadoe <aashuaripirala@gmail.com>
2023-04-29 21:10:22 +0530

    10 split

--- mnist.py
+++ mnist.py
@@ -36,37 +36,43 @@ activation_functions = ['tanh', 'relu', 'sigmoid']
 output_functions = ['tanh', 'sigmoid', 'softmax']
 
 results = []
+kf = KFold(n_splits=10, shuffle=True)
 
-for i, architecture in enumerate(architectures):
-    if architecture['num_hidden_layers'] == 2:
-        num_neurons = [50, 50] if random.random() == 0.5 else [30, 70] if random.random() < 0.5 else [70, 30]
-    else:
-        num_neurons = [25, 25, 50] if random.random() == 0.5 else [25, 50, 25] if random.random() < 0.5 else [50, 25, 25]
-        
-    architecture['hidden_layer_size'] = num_neurons
-    
-    for activation in activation_functions:
-        for output_fn in output_functions:
-            model = Sequential()
-            model.add(Conv2D(filters=32, kernel_size=(3, 3), activation=activation, input_shape=(28, 28, 1)))
-            model.add(MaxPooling2D(pool_size=(2, 2)))
-            model.add(Flatten())
-            for i in range(architecture['num_hidden_layers']):
-                model.add(Dense(units=num_neurons[i], activation=activation))
-            model.add(Dense(units=num_classes, activation=output_fn))               
-            model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) 
-            model.fit(X_train, y_train, batch_size=batch_size, epochs=num_epochs, verbose=0)
-            y_predict = np.argmax(model.predict(X_test), axis=1)
-            accuracy = accuracy_score(np.argmax(y_test, axis=1), y_pred=y_predict)
-            cm = confusion_matrix(np.argmax(y_test, axis=1), y_pred=y_predict)
+
+
+for train_idx, test_idx in kf.split(X_train):
+    X_cv_train, X_cv_test = X_train[train_idx], X_train[test_idx]
+    y_cv_train, y_cv_test = y_train[train_idx], y_test[test_idx]
+    for i, architecture in enumerate(architectures):
+        if architecture['num_hidden_layers'] == 2:
+            num_neurons = [50, 50] if random.random() == 0.5 else [30, 70] if random.random() < 0.5 else [70, 30]
+        else:
+            num_neurons = [25, 25, 50] if random.random() == 0.5 else [25, 50, 25] if random.random() < 0.5 else [50, 25, 25]
             
-            results.append({
-                'architecture': architecture,
-                'activation function': activation,
-                'accuracy': accuracy,
-                'confusion matrix': cm,
-                'output function': output_fn
-            })
+        architecture['hidden_layer_size'] = num_neurons
+        
+        for activation in activation_functions:
+            for output_fn in output_functions:
+                model = Sequential()
+                model.add(Conv2D(filters=32, kernel_size=(3, 3), activation=activation, input_shape=(28, 28, 1)))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+                model.add(Flatten())
+                for i in range(architecture['num_hidden_layers']):
+                    model.add(Dense(units=num_neurons[i], activation=activation))
+                model.add(Dense(units=num_classes, activation=output_fn))               
+                model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy']) 
+                model.fit(X_train, y_train, batch_size=batch_size, epochs=num_epochs, verbose=0)
+                y_predict = np.argmax(model.predict(X_test), axis=1)
+                accuracy = accuracy_score(np.argmax(y_test, axis=1), y_pred=y_predict)
+                cm = confusion_matrix(np.argmax(y_test, axis=1), y_pred=y_predict)
+                
+                results.append({
+                    'architecture': architecture,
+                    'activation function': activation,
+                    'accuracy': accuracy,
+                    'confusion matrix': cm,
+                    'output function': output_fn
+                })
             
 for i, result in enumerate(results):
     print(f'Model {i+1}: Architecture={result["architecture"]}, Activation Function={result["activation function"]}, Output Function={result["output function"]},Accuracy={result["accuracy"]:.4f}')
